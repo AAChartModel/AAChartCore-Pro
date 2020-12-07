3450cb7d8fc8910f9a7eef41a03b4d701cbbd8f5
AnAn <2236368544@qq.com>
2020-12-07 18:05:52 +0800

    Add json files resources

--- app/src/main/java/com/aachartmodel/aachartcore_pro/MainActivity.java
+++ app/src/main/java/com/aachartmodel/aachartcore_pro/MainActivity.java
@@ -2,7 +2,14 @@ package com.aachartmodel.aachartcore_pro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
+import android.content.Context;
 import android.os.Bundle;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        String testJson = getJson("data/bellcurveData.json",this);
+        String testJson1 = getJson("data/bellcurveData.json",this);
+        String testJson2 = getJson("data/bellcurveData.json",this);
+
+    }
+
+    public static String getJson(String fileName, Context context){
+        StringBuilder stringBuilder = new StringBuilder();
+        try {
+            InputStream is = context.getAssets().open(fileName);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line=bufferedReader.readLine()) != null){
+                stringBuilder.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return stringBuilder.toString();
     }
 }
