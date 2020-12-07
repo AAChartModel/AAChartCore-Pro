63b23dca70f76826160e4c720b1fc0672abdc71a
AnAn <2236368544@qq.com>
2020-12-07 17:30:01 +0800

    Create AAOptionsData.java

---
+++ app/src/main/java/com/aachartmodel/aachartcore_pro/AAOptionsData.java
@@ -0,0 +1,137 @@
+package com.aachartmodel.aachartcore_pro;
+
+class AAOptionsData {
+
+    public static Object[] variablepieData;
+    public static Object[] variwideData;
+    public static Object[] heatmapData;
+    public static Object[] packedbubbleData;
+    public static Object[] packedbubbleSplitData;
+    public static Object[] columnpyramidData;
+    public static Object[] treemapWithColorAxisData;
+    public static Object[] drilldownTreemapData;
+
+    public static Object[] sankeyData;
+    public static Object[] dependencywheelData;
+    public static Object[] sunburstData;
+    public static Object[] dumbbellData;
+    public static Object[] vennData;
+    public static Object[] lollipopData;
+    public static Object[] tilemapData;
+    public static Object[] treemapWithLevelsData;
+    public static Object[] xrangeData;
+    public static Object[] vectorData;
+    public static Object[] bellcurveData;
+    public static Object[] timelineData;
+    public static Object[] itemData;
+    public static Object[] windbarbData;
+    public static Object[] networkgraphData;
+    public static Object[] wordcloudData;
+    public static Object[] eulerData;
+
+    private static Object[] getVariablepieData() {
+        return getJsonDataWithJsonFileName("variablepieData");
+    }
+
+    private static Object[] getVariwideData() {
+        return getJsonDataWithJsonFileName("variwideData");
+    }
+
+    private static Object[] getHeatmapData() {
+        return getJsonDataWithJsonFileName("heatmapData");
+    }
+
+    private static Object[] getPackedbubbleData() {
+        return getJsonDataWithJsonFileName("packedbubbleData");
+    }
+
+    private static Object[] getPackedbubbleSplitData() {
+        return getJsonDataWithJsonFileName("packedbubbleSplitData");
+    }
+
+    private static Object[] getColumnpyramidData() {
+        return getJsonDataWithJsonFileName("columnpyramidData");
+    }
+
+    private static Object[] getTreemapWithColorAxisData() {
+        return getJsonDataWithJsonFileName("treemapWithColorAxisData");
+    }
+
+    private static Object[] getDrilldownTreemapData() {
+        return getJsonDataWithJsonFileName("drilldownTreemapData");
+    }
+
+    private static Object[] getSankeyData() {
+        return getJsonDataWithJsonFileName("sankeyData");
+    }
+
+    private static Object[] getDependencywheelData() {
+        return getJsonDataWithJsonFileName("dependencywheelData");
+    }
+
+    private static Object[] getSunburstData() {
+        return getJsonDataWithJsonFileName("sunburstData");
+    }
+
+    private static Object[] getDumbbellData() {
+        return getJsonDataWithJsonFileName("dumbbellData");
+    }
+
+    private static Object[] getVennData() {
+        return getJsonDataWithJsonFileName("vennData");
+    }
+
+    private static Object[] getLollipopData() {
+        return getJsonDataWithJsonFileName("lollipopData");
+    }
+
+    private static Object[] getTilemapData() {
+        return getJsonDataWithJsonFileName("tilemapData");
+    }
+
+    private static Object[] getTreemapWithLevelsData() {
+        return getJsonDataWithJsonFileName("treemapWithLevelsData");
+    }
+
+    private static Object[] getXrangeData() {
+        return getJsonDataWithJsonFileName("xrangeData");
+    }
+
+    private static Object[] getVectorData() {
+        return getJsonDataWithJsonFileName("vectorData");
+    }
+
+    private static Object[] getBellcurveData() {
+        return getJsonDataWithJsonFileName("bellcurveData");
+    }
+
+    private static Object[] getTimelineData() {
+        return getJsonDataWithJsonFileName("timelineData");
+    }
+
+    private static Object[] getItemData() {
+        return getJsonDataWithJsonFileName("itemData");
+    }
+
+    private static Object[] getWindbarbData() {
+        return getJsonDataWithJsonFileName("windbarbData");
+    }
+
+    private static Object[] getNetworkgraphData() {
+        return getJsonDataWithJsonFileName("networkgraphData");
+    }
+
+    private static Object[] getWordcloudData() {
+        return getJsonDataWithJsonFileName("wordcloudData");
+    }
+
+    private static Object[] getEulerData() {
+        return getJsonDataWithJsonFileName("eulerData");
+    }
+
+
+
+    private static Object[] getJsonDataWithJsonFileName(String jsonFileName) {
+        return new Object[]{};
+    }
+}
