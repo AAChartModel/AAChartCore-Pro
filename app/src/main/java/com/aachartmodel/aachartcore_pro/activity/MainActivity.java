cc9e1ea7e3446181ddb472c95c06e57f9764f95c
AnAn <2236368544@qq.com>
2020-12-11 17:48:54 +0800

    Remove useless code

--- app/src/main/java/com/aachartmodel/aachartcore_pro/activity/MainActivity.java
+++ app/src/main/java/com/aachartmodel/aachartcore_pro/activity/MainActivity.java
@@ -19,9 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Object[] packedbubbleSeries = AAOptionsSeriesTool.getPackedbubbleSeries();
-        Object[] streamgraphSeries = AAOptionsSeriesTool.getStreamgraphSeries();
-
+ 
         setupRecycleView();
     }
 
