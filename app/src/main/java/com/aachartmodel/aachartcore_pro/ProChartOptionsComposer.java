9fe997c9879cc594c6bf644ea325d46397a76829
An An <2236368544@qq.com>
2022-04-10 19:35:12 +0800

    Update ProChartOptionsComposer.java

--- app/src/main/java/com/aachartmodel/aachartcore_pro/ProChartOptionsComposer.java
+++ app/src/main/java/com/aachartmodel/aachartcore_pro/ProChartOptionsComposer.java
@@ -791,6 +791,222 @@ public class ProChartOptionsComposer {
                 });
     }
 
+    public static AAOptions organizationChart() {
+            return new AAOptions()
+                    .chart(new AAChart()
+//            .height(600)
+                            .inverted(true))
+                    .title(new AATitle()
+                            .text("Highsoft 公司组织结构"))
+                    .series(new AASeriesElement[]{
+                            new AASeriesElement()
+                                    .type(AAChartType.Organization)
+                                    .name("Highsoft")
+                                    .keys(new String[]{"from", "to"})
+                                    .data(AAOptionsData.organizationData)
+                                    .levels(new AALevelsElement[]{
+                                            new AALevelsElement()
+                                                    .level(0)
+                                                    .color("silver")
+                                                    .dataLabels(new AADataLabels()
+                                                    .color(AAColor.Black))
+//                            .height(25)
+                                            ,
+                                            new AALevelsElement()
+                                                    .level(1)
+                                                    .color("silver")
+                                                    .dataLabels(new AADataLabels()
+                                                    .color(AAColor.Black))
+//                            .height(25)
+                                            ,
+                                            new AALevelsElement()
+                                                    .level(2)
+                                                    .color("#980104")
+                                            ,
+                                            new AALevelsElement()
+                                                    .level(4)
+                                                    .color("#359154")
+                                    })
+//                .nodes(AAOptionsData.organizationNodesData)
+                                    .colorByPoint(false)
+                                    .color("#007ad0")
+                                    .dataLabels(new AADataLabels()
+                                    .color(AAColor.White))
+//                    .borderColor(AAColor.White)
+//                    .nodeWidth(65)
+                    })
+        .tooltip(new AATooltip()
+//                    .outside(true)
+        );
+        }
+
+    //https://www.highcharts.com/docs/chart-and-series-types/arc-diagram
+    public static AAOptions arcdiagramChart1() {
+        return new AAOptions()
+                .colors(new String[]{"#293462", "#a64942", "#fe5f55", "#fff1c1", "#5bd1d7", "#ff502f", "#004d61", "#ff8a5c", "#fff591", "#f5587b", "#fad3cf", "#a696c8", "#5BE7C4", "#266A2E", "#593E1A"})
+        .title(new AATitle()
+                .text("Main train connections in Europe"))
+                .series(new AASeriesElement[]{
+                        new AASeriesElement()
+                                .keys(new String[]{"from", "to", "weight"})
+                                .type(AAChartType.Arcdiagram)
+                                .name("Train connections")
+//                .linkWeight(1)
+//                .centeredLinks(true)
+                                .dataLabels(new AADataLabels()
+                                        .rotation(90f)
+                                        .y(30f)
+                                        .align(AAChartAlignType.Left)
+                                        .color(AAColor.Black))
+//                .offset("65%")
+                                .data(AAOptionsData.arcdiagram1Data)
+                });
+    }
+
+    public static AAOptions arcdiagramChart2() {
+        return new AAOptions()
+                .title(new AATitle()
+                        .text("Highcharts Arc Diagram"))
+                .subtitle(new AASubtitle()
+                        .text("Arc Diagram with marker symbols"))
+                .series(new AASeriesElement[]{
+                        new AASeriesElement()
+//                                .linkWeight(1)
+                                .keys(new String[]{"from", "to", "weight"})
+                .type(AAChartType.Arcdiagram)
+                .marker(new AAMarker()
+                        .symbol(AAChartSymbolType.Triangle)
+                        .lineWidth(2f)
+                        .lineColor(AAColor.White))
+//                .centeredLinks(true)
+                .dataLabels(new AADataLabels()
+                        .format("{point.fromNode.name} → {point.toNode.name}")
+//                        .nodeFormat("{point.name}")
+                        .color(AAColor.Black)
+//                        .linkTextPath(new AATextPath()
+//                                .enabled(true))
+                )
+                .data(AAOptionsData.arcdiagram2Data)
+            });
+    }
+
+    public static AAOptions arcdiagramChart3() {
+        return new AAOptions()
+                .chart(new AAChart()
+                        .inverted(true))
+                .title(new AATitle()
+                        .text("Highcharts Inverted Arc Diagram"))
+                .series(new AASeriesElement[]{
+                        new AASeriesElement()
+                                .keys(new String[]{"from", "to", "weight"})
+//                .centerPos("50%")
+                                .type(AAChartType.Arcdiagram)
+                                .dataLabels(new AADataLabels()
+                                                .align(AAChartAlignType.Right)
+                                                .x(-20f)
+                                                .y(-2f)
+                                                .color("#333333")
+                                                .overflow("allow")
+//                        .padding(0)
+                                )
+//                .offset("60%")
+                                .data(AAOptionsData.arcdiagram3Data)
+                });
+    }
+
+    public static AAOptions flameChart() {
+        return new AAOptions()
+                .chart(new AAChart()
+                        .inverted(true))
+                .title(new AATitle()
+                        .align(AAChartAlignType.Left)
+                        .text("Flame chart (layout: flame)"))
+                .subtitle(new AASubtitle()
+                        .align(AAChartAlignType.Left)
+                        .text("Highcharts chart rendering process"))
+                .legend(new AALegend()
+                        .enabled(false))
+                .xAxisArray(new AAXAxis[]{
+                        new AAXAxis()
+                                .visible(false),
+                        new AAXAxis()
+                                .visible(false)
+                                .startOnTick(false)
+//                                .endOnTick(false)
+//                                .minPadding(0)
+//                                .maxPadding(0)
+                })
+        .yAxisArray(new AAYAxis[]{
+                new AAYAxis()
+                        .visible(false),
+                new AAYAxis()
+                        .visible(false)
+                        .min(0f)
+//                        .maxPadding(0)
+//                        .startOnTick(false)
+//                        .endOnTick(false)
+        })
+        .series(new AASeriesElement[]{
+                new AASeriesElement()
+                        .type(AAChartType.Flame)
+                        .data(AAOptionsData.flameData)
+                        .yAxis(1)
+                        .xAxis(1),
+                new AASeriesElement()
+//                        .visible(false)
+                        .size("100%")
+                        .type(AAChartType.Sunburst)
+                        .data(AAOptionsData.sunburst2Data)
+//                        .allowDrillToNode(true)
+//                        .cursor("pointer")
+                        .levels(new AALevelsElement[]{
+                                new AALevelsElement()
+                                        .level(1)
+//                        .levelIsConstant(false)
+                                        .dataLabels(new AADataLabels()
+                                        .enabled(false))
+                        })
+                        .dataLabels(new AADataLabels()
+                        .textPath(new AATextPath()
+//                        .attributes({"dy": 5})
+                                        .enabled(true)
+                        ))
+        })
+        .tooltip(new AATooltip()
+                .headerFormat("")
+                .pointFormat("selfSize of {point.name} is {point.value}"));
+    }
+
+
+    public static AAOptions packedbubbleSpiralChart() {
+        return new AAOptions()
+                .chart(new AAChart()
+                                .type(AAChartType.Packedbubble)
+//            .height("100%")
+                )
+                .title(new AATitle()
+                        .text("Carbon emissions around the world (2014)"))
+                .tooltip(new AATooltip()
+                        .useHTML(true)
+                        .pointFormat("{point.name}: {point.y}m CO2"))
+                .plotOptions(new AAPlotOptions()
+                        .packedbubble(new AAPackedbubble()
+//                                .useSimulation(false)
+                                .minSize("20%")
+                                .maxSize("80%")
+                                .dataLabels(new AADataLabels()
+                                        .enabled(true)
+                                        .format("{point.name}")
+                                        .filter(new AAFilter()
+                                                .property("y")
+                                                .operator(">")
+                                                .value(250f))
+                                        .style(new AAStyle()
+                                                .color(AAColor.Black)
+                                                .textOutline("none")
+                                                .fontWeight("normal")))))
+                .series(AAOptionsSeries.packedbubbleSeries);
+    }
 
 
 }
