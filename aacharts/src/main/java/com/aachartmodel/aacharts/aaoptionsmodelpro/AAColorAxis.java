a698dabf94f28b1ea73691b203a50d9ef4d9b7c5
An An <2236368544@qq.com>
2022-10-22 16:27:58 +0800

    Update AAColorAxis.java

--- aacharts/src/main/java/com/aachartmodel/aacharts/aaoptionsmodelpro/AAColorAxis.java
+++ aacharts/src/main/java/com/aachartmodel/aacharts/aaoptionsmodelpro/AAColorAxis.java
@@ -1,10 +1,28 @@
 package com.aachartmodel.aacharts.aaoptionsmodelpro;
 
+import com.aachartmodel.aacharts.aaoptionsmodel.AALabels;
+
 public class AAColorAxis {
+    //AAPropStatementAndPropSetFuncStatement(strong, AAColorAxis, NSNumber *, min)
+    //AAPropStatementAndPropSetFuncStatement(copy,   AAColorAxis, NSString *, minColor)
+    //AAPropStatementAndPropSetFuncStatement(copy,   AAColorAxis, NSString *, maxColor)
+    //AAPropStatementAndPropSetFuncStatement(strong, AAColorAxis, NSArray<AADataClassesElement *> *, dataClasses)
+    //AAPropStatementAndPropSetFuncStatement(strong, AAColorAxis, NSArray *, stops)
+    //AAPropStatementAndPropSetFuncStatement(strong, AAColorAxis, NSNumber *, max)
+    //AAPropStatementAndPropSetFuncStatement(assign, AAColorAxis, BOOL      , startOnTick)
+    //AAPropStatementAndPropSetFuncStatement(assign, AAColorAxis, BOOL      , endOnTick)
+    //AAPropStatementAndPropSetFuncStatement(strong, AAColorAxis, AALabels *, labels)
+
     public Float min;
     public String minColor;
     public String maxColor;
     public AADataClassesElement[] dataClasses;
+    public Object[] stops;
+    public Float max;
+    public Boolean startOnTick;
+    public Boolean endOnTick;
+    public AALabels labels;
+
 
 
     public AAColorAxis min(Float prop) {
@@ -24,6 +42,31 @@ public class AAColorAxis {
 
     public AAColorAxis dataClasses(AADataClassesElement[] prop) {
         dataClasses = prop;
+        return this;
+    }
+
+    public AAColorAxis stops(Object[] prop) {
+        stops = prop;
+        return this;
+    }
+
+    public AAColorAxis max(Float prop) {
+        max = prop;
+        return this;
+    }
+
+    public AAColorAxis startOnTick(Boolean prop) {
+        startOnTick = prop;
+        return this;
+    }
+
+    public AAColorAxis endOnTick(Boolean prop) {
+        endOnTick = prop;
+        return this;
+    }
+
+    public AAColorAxis labels(AALabels prop) {
+        labels = prop;
         return this;
     }
     
