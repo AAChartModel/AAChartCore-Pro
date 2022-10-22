d320e47e168f0f692564eaf71990fdc64be926be
An An <2236368544@qq.com>
2022-10-22 15:58:58 +0800

    Update AAColumnVariantChartComposer.java

--- app/src/main/java/com/aachartmodel/aachartcore_pro/composer/AAColumnVariantChartComposer.java
+++ app/src/main/java/com/aachartmodel/aachartcore_pro/composer/AAColumnVariantChartComposer.java
@@ -15,6 +15,7 @@ import com.aachartmodel.aacharts.aaoptionsmodel.AADataLabels;
 import com.aachartmodel.aacharts.aaoptionsmodel.AALegend;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAMarker;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAOptions;
+import com.aachartmodel.aacharts.aaoptionsmodel.AAPlotBandsElement;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAPlotOptions;
 import com.aachartmodel.aacharts.aaoptionsmodel.AASeries;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAStyle;
@@ -23,6 +24,7 @@ import com.aachartmodel.aacharts.aaoptionsmodel.AATitle;
 import com.aachartmodel.aacharts.aaoptionsmodel.AATooltip;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAXAxis;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAYAxis;
+import com.aachartmodel.aacharts.aaoptionsmodelpro.AABulletDataElement;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AAColorAxis;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AAColorVariation;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AADataClassesElement;
@@ -235,6 +237,90 @@ public class AAColumnVariantChartComposer {
                 });
     }
 
+    //+ (AAOptions *)histogramChart {
+    //    return AAOptions.new
+    //        .titleSet(AATitle.new
+    //            .textSet(@"AAChartKit-Pro Histogram"))
+    //        .xAxisSet((id)@[
+    //            AAXAxis.new
+    //                .titleSet(AAAxisTitle.new
+    //                    .textSet(@"Data")),
+    //            AAXAxis.new
+    //                .titleSet(AAAxisTitle.new
+    //                    .textSet(@"Histogram"))
+    //                .oppositeSet(true)
+    //            ])
+    //        .yAxisSet((id)@[
+    //            AAYAxis.new
+    //                .titleSet(AAAxisTitle.new
+    //                    .textSet(@"Data")),
+    //            AAYAxis.new
+    //                .titleSet(AAAxisTitle.new
+    //                    .textSet(@"Histogram"))
+    //                .oppositeSet(true)
+    //            ])
+    //        .seriesSet(@[
+    //            AASeriesElement.new
+    //                .nameSet(@"Histogram")
+    //                .typeSet(AAChartTypeHistogram)
+    //                .xAxisSet(@1)
+    //                .yAxisSet(@1)
+    //                .baseSeriesSet((id)@"s1")
+    //                .zIndexSet(@-1),
+    //            AASeriesElement.new
+    //                .nameSet(@"Data")
+    //                .typeSet(AAChartTypeScatter)
+    //                .dataSet(AAOptionsData.bellcurveData)
+    //                .idSet(@"s1")
+    //                .markerSet(AAMarker.new
+    //                           .fillColorSet(AAColor.whiteColor)//点的填充色(用来设置折线连接点的填充色)
+    //                           .lineWidthSet(@2)//外沿线的宽度(用来设置折线连接点的轮廓描边的宽度)
+    //                           .lineColorSet(@""))//外沿线的颜色(用来设置折线连接点的轮廓描边颜色，当值为空字符串时，默认取数据点或数据列的颜色))
+    //            ]);
+    //}
+
+    public static AAOptions histogramChart() {
+        return new AAOptions()
+                .title(new AATitle()
+                        .text("AAChartKit-Pro Histogram"))
+                .xAxisArray(new AAXAxis[]{
+                        new AAXAxis()
+                                .title(new AAAxisTitle()
+                                        .text("Data")),
+                        new AAXAxis()
+                                .title(new AAAxisTitle()
+                                        .text("Histogram"))
+                                .opposite(true)
+                })
+                .yAxisArray(new AAYAxis[]{
+                        new AAYAxis()
+                                .title(new AAAxisTitle()
+                                        .text("Data")),
+                        new AAYAxis()
+                                .title(new AAAxisTitle()
+                                        .text("Histogram"))
+                                .opposite(true)
+                })
+                .series(new AASeriesElement[]{
+                        new AASeriesElement()
+                                .name("Histogram")
+                                .type(AAChartType.Histogram)
+                                .xAxis(1)
+                                .yAxis(1)
+//                                .baseSeries("s1")
+                                .zIndex(-1),
+                        new AASeriesElement()
+                                .name("Data")
+                                .type(AAChartType.Scatter)
+                                .data(AAOptionsData.bellcurveData)
+//                                .id("s1")
+                                .marker(new AAMarker()
+                                        .fillColor(AAColor.White)//点的填充色(用来设置折线连接点的填充色)
+                                        .lineWidth(2f)//外沿线的宽度(用来设置折线连接点的轮廓描边的宽度)
+                                        .lineColor(""))//外沿线的颜色(用来设置折线连接点的轮廓描边颜色，当值为空字符串时，默认取数据点或数据列的颜色))
+                });
+    }
+
     public static AAOptions bellcurveChart() {
         return new AAOptions()
                 .title(new AATitle()
@@ -272,6 +358,110 @@ public class AAColumnVariantChartComposer {
                                         .lineColor(""))//外沿线的颜色(用来设置折线连接点的轮廓描边颜色，当值为空字符串时，默认取数据点或数据列的颜色))
                                 .data(AAOptionsData.bellcurveData)
                 });
+    }
+
+    //+ (AAOptions *)bulletChart {
+    //   return AAOptions.new
+    //        .chartSet(AAChart.new
+    //            .marginTopSet(@40)
+    //            .invertedSet(true)
+    ////            .marginLeftSet(@135)
+    //            .heightSet(@200)
+    //            .typeSet(AAChartTypeBullet))
+    //        .titleSet(AATitle.new
+    //            .textSet(@"2017 年公司运营情况"))
+    //        .xAxisSet(AAXAxis.new
+    //            .categoriesSet(@[@"<span style=color:#ff0000;font-weight:bold;font-size:13px>营收</span><br/>千美元", ]))
+    //        .yAxisSet(AAYAxis.new
+    //            .gridLineWidthSet(@0)
+    //            .plotBandsSet(@[
+    //                AAPlotBandsElement.new
+    //                    .fromSet(@0)
+    //                    .toSet(@150)
+    //                    .colorSet(@"#666"),
+    //                AAPlotBandsElement.new
+    //                    .fromSet(@150)
+    //                    .toSet(@225)
+    //                    .colorSet(@"#999"),
+    //                AAPlotBandsElement.new
+    //                    .fromSet(@225)
+    //                    .toSet(@9000000000)
+    //                    .colorSet(@"#bbb")
+    //                ])
+    //            .titleSet((id)NSNull.new))
+    //        .plotOptionsSet(AAPlotOptions.new
+    //            .seriesSet(AASeries.new
+    //                .pointPaddingSet(@0.25)
+    //                .borderWidthSet(@0)
+    ////                .colorSet(@"#000")
+    ////                .targetOptionsSet(AATargetOptions.new
+    ////                    .widthSet(@"200%"))
+    //                       ))
+    //        .legendSet(AALegend.new
+    //            .enabledSet(false))
+    //        .seriesSet(@[
+    //            AASeriesElement.new
+    //                .dataSet(@[
+    //                    AABulletDataElement.new
+    //                        .ySet(@275)
+    //                        .targetSet(@250)
+    //                    ])
+    //            ])
+    //        .tooltipSet(AATooltip.new
+    //            .pointFormatSet(@"{point.y} （目标值 {point.target}）"));
+    //}
+
+    public static AAOptions bulletChart() {
+        return new AAOptions()
+                .chart(new AAChart()
+                        .marginTop(40f)
+                        .inverted(true)
+//                        .height(200f)
+                        .type(AAChartType.Bullet))
+                .title(new AATitle()
+                        .text("2017 年公司运营情况"))
+                .xAxis(new AAXAxis()
+                        .categories(new String[]{
+                                "<span style=color:#ff0000;font-weight:bold;font-size:13px>营收</span><br/>千美元",
+                        }))
+                .yAxis(new AAYAxis()
+                        .gridLineWidth(0f)
+                        .plotBands(new AAPlotBandsElement[]{
+                                new AAPlotBandsElement()
+                                        .from(0f)
+                                        .to(150f)
+                                        .color("#666"),
+                                new AAPlotBandsElement()
+                                        .from(150f)
+                                        .to(225f)
+                                        .color("#999"),
+                                new AAPlotBandsElement()
+                                        .from(225f)
+                                        .to(9000000000f)
+                                        .color("#bbb"),
+                        })
+//                        .title((Object) null)
+                )
+                .plotOptions(new AAPlotOptions()
+                        .series(new AASeries()
+                                .pointPadding(0.25f)
+//                                .borderWidth(0f)
+//                                .color("#000")
+//                                .targetOptions(new AATargetOptions()
+//                                        .width("200%"))
+                        ))
+                .legend(new AALegend()
+                        .enabled(false))
+                .series(new AASeriesElement[]{
+                        new AASeriesElement()
+                                .data(new AABulletDataElement[]{
+                                        new AABulletDataElement()
+                                                .y(275f)
+                                                .target(250f),
+                                })
+                })
+                .tooltip(new AATooltip()
+                        .pointFormat("{point.y} （目标值 {point.target}）"));
     }
 
 }
