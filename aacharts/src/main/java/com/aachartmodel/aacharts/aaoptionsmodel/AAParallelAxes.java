777b1b242751787c22660aea91582fe64c3e7ff2
An An <2236368544@qq.com>
2022-10-22 18:11:52 +0800

    Create AAParallelAxes.java

---
+++ aacharts/src/main/java/com/aachartmodel/aacharts/aaoptionsmodel/AAParallelAxes.java
@@ -0,0 +1,12 @@
+package com.aachartmodel.aacharts.aaoptionsmodel;
+
+public class AAParallelAxes {
+//    AAPropStatementAndPropSetFuncStatement(strong, AAParallelAxes, NSNumber  *, lineWidth)
+
+    public Number lineWidth;
+
+     public AAParallelAxes lineWidth(Number prop) {
+         lineWidth = prop;
+         return this;
+     }
+}
