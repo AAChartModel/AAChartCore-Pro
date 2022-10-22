af0ae32c946599df0cef419a865e0d06dd35f2c0
An An <2236368544@qq.com>
2022-10-22 16:28:21 +0800

    Update AAHeatOrTreeMapChartComposer.java

--- app/src/main/java/com/aachartmodel/aachartcore_pro/composer/AAHeatOrTreeMapChartComposer.java
+++ app/src/main/java/com/aachartmodel/aachartcore_pro/composer/AAHeatOrTreeMapChartComposer.java
@@ -1,9 +1,11 @@
 package com.aachartmodel.aachartcore_pro.composer;
 
+import com.aachartmodel.aachartcore_pro.datasource.AAOptionsCSV;
 import com.aachartmodel.aachartcore_pro.datasource.AAOptionsData;
 import com.aachartmodel.aachartcore_pro.datasource.AAOptionsSeries;
 import com.aachartmodel.aacharts.aachartcreator.AASeriesElement;
 import com.aachartmodel.aacharts.aachartenum.AAChartAlignType;
+import com.aachartmodel.aacharts.aachartenum.AAChartAxisType;
 import com.aachartmodel.aacharts.aachartenum.AAChartFontWeightType;
 import com.aachartmodel.aacharts.aachartenum.AAChartLayoutType;
 import com.aachartmodel.aacharts.aachartenum.AAChartSymbolType;
@@ -12,6 +14,7 @@ import com.aachartmodel.aacharts.aachartenum.AAChartVerticalAlignType;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAAxisTitle;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAChart;
 import com.aachartmodel.aacharts.aaoptionsmodel.AADataLabels;
+import com.aachartmodel.aacharts.aaoptionsmodel.AALabels;
 import com.aachartmodel.aacharts.aaoptionsmodel.AALegend;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAMarker;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAOptions;
@@ -25,8 +28,10 @@ import com.aachartmodel.aacharts.aaoptionsmodel.AAXAxis;
 import com.aachartmodel.aacharts.aaoptionsmodel.AAYAxis;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AAColorAxis;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AAColorVariation;
+import com.aachartmodel.aacharts.aaoptionsmodelpro.AAData;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AADataClassesElement;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AAFilter;
+import com.aachartmodel.aacharts.aaoptionsmodelpro.AAHeatmap;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AAItem;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AALayoutAlgorithm;
 import com.aachartmodel.aacharts.aaoptionsmodelpro.AALevelsElement;
@@ -37,6 +42,7 @@ import com.aachartmodel.aacharts.aatools.AAColor;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class AAHeatOrTreeMapChartComposer {
     public static AAOptions treemapWithLevelsData() {
@@ -162,4 +168,226 @@ public class AAHeatOrTreeMapChartComposer {
                 });
     }
 
-}
+    //+ (AAOptions *)treemapWithColorAxisData {
+    //    return AAOptions.new
+    //    .chartSet(AAChart.new
+    //              .typeSet(AAChartTypeTreemap))
+    //    .titleSet(AATitle.new
+    //              .textSet(@"矩形树图"))
+    //    .colorAxisSet(AAColorAxis.new
+    //                  .minColorSet(AAColor.whiteColor)
+    //                  .maxColorSet(AAColor.redColor))
+    //    .seriesSet(@[
+    //        AASeriesElement.new
+    //        .dataSet(AAOptionsData.treemapWithColorAxisData)
+    //               ]);
+    //}
+
+    public static AAOptions treemapWithColorAxisData() {
+        return new AAOptions()
+                .chart(new AAChart()
+                        .type(AAChartType.Treemap))
+                .title(new AATitle()
+                        .text("矩形树图"))
+                .colorAxis(new AAColorAxis()
+                        .minColor(AAColor.White)
+                        .maxColor(AAColor.Red))
+                .series(new AASeriesElement[]{
+                        new AASeriesElement()
+                                .data(AAOptionsData.treemapWithColorAxisData)
+                });
+    }
+
+    //+ (AAOptions *)drilldownLargeDataTreemapChart {
+    //    return AAOptions.new
+    //    .chartSet(AAChart.new
+    //              .typeSet(AAChartTypeTreemap))
+    //    .titleSet(AATitle.new
+    //              .textSet(@"2012年，全球每10w人口死亡率"))
+    //    .subtitleSet(AASubtitle.new
+    //                 .textSet(@"点击下钻"))
+    //    .plotOptionsSet(AAPlotOptions.new
+    //                    .treemapSet(AATreemap.new
+    //                                .allowTraversingTreeSet(true)
+    //                                .layoutAlgorithmSet(@"squarified")))
+    //    .seriesSet(@[
+    //        AASeriesElement.new
+    //        .typeSet(AAChartTypeTreemap)
+    //        .levelsSet(@[
+    //            AALevelsElement.new
+    //            .levelSet(@1)
+    //            .dataLabelsSet(AADataLabels.new
+    //                           .enabledSet(true))
+    //            .borderWidthSet(@3)
+    //                   ])
+    //        .dataSet(AAOptionsData.drilldownTreemapData)
+    //               ]);
+    //}
+
+    public static AAOptions drilldownLargeDataTreemapChart() {
+        return new AAOptions()
+                .chart(new AAChart()
+                        .type(AAChartType.Treemap))
+                .title(new AATitle()
+                        .text("2012年，全球每10w人口死亡率"))
+                .subtitle(new AASubtitle()
+                        .text("点击下钻"))
+                .plotOptions(new AAPlotOptions()
+                        .treemap(new AATreemap()
+                                .allowTraversingTree(true)
+                                .layoutAlgorithm("squarified")))
+                .series(new AASeriesElement[]{
+                        new AASeriesElement()
+                                .type(AAChartType.Treemap)
+                                .levels(new AALevelsElement[]{
+                                        new AALevelsElement()
+                                                .level(1)
+                                                .dataLabels(new AADataLabels()
+                                                        .enabled(true))
+                                                .borderWidth(3f)
+                                })
+                                .data(AAOptionsData.drilldownTreemapData)
+                });
+    }
+
+    //+ (AAOptions *)largeDataHeatmapChart {
+    //    NSString *csvStr = AAOptionsCSV.csvData[@"csv"];
+    //    return AAOptions.new
+    //        .dataSet(AAData.new
+    //                 .csvSet([self aa_toPureJSString2WithString:csvStr])
+    //                 .parsedSet(@AAJSFunc(function () {
+    //                     start = +new Date();
+    //                 })))
+    //        .chartSet(AAChart.new
+    //            .typeSet(AAChartTypeHeatmap)
+    //            .marginSet(@[@60, @10, @80, @50]))
+    //        .titleSet(AATitle.new
+    //            .textSet(@"大型热力图")
+    //            .alignSet(AAChartAlignTypeLeft)
+    //            .xSet(@40))
+    //        .subtitleSet(AASubtitle.new
+    //            .textSet(@"2013每天每小时的热力变化")
+    //            .alignSet(AAChartAlignTypeLeft)
+    //            .xSet(@40))
+    //        .xAxisSet(AAXAxis.new
+    //            .typeSet(AAChartAxisTypeDatetime)
+    //            .minSet(@1356998400000)
+    //            .maxSet(@1388534400000)
+    //            .labelsSet(AALabels.new
+    //                .alignSet(AAChartAlignTypeLeft)
+    //                .xSet(@5)
+    //                .ySet(@14)
+    //                .formatSet(@"{value:%B}"))
+    ////            .showLastLabelSet(false)
+    //            .tickLengthSet(@16))
+    //        .yAxisSet(AAYAxis.new
+    //            .titleSet(AAAxisTitle.new
+    //                .textSet((id)NSNull.new))
+    //            .labelsSet(AALabels.new
+    //                .formatSet(@"{value}:00"))
+    //            .minPaddingSet(@0)
+    //            .maxPaddingSet(@0)
+    //            .startOnTickSet(false)
+    //            .endOnTickSet(false)
+    //            .tickPositionsSet(@[@0, @6, @12, @18, @24])
+    //            .tickWidthSet(@1)
+    //            .minSet(@0)
+    //            .maxSet(@23)
+    //            .reversedSet(true))
+    //        .colorAxisSet(AAColorAxis.new
+    //            .stopsSet(@[
+    //                @[@0, @"#3060cf", ],
+    //                @[@0.5, @"#fffbbc", ],
+    //                @[@0.9, @"#c4463a", ],
+    //                @[@1, @"#c4463a", ]
+    //                ])
+    //            .minSet(@-15)
+    //            .maxSet(@25)
+    //            .startOnTickSet(false)
+    //            .endOnTickSet(false)
+    //            .labelsSet(AALabels.new
+    //                .formatSet(@"{value}℃"))
+    //                      )
+    //        .seriesSet(@[
+    //            AAHeatmap.new
+    //                .borderWidthSet(@0)
+    //                .colsizeSet(@86400000)
+    //                .tooltipSet(AATooltip.new
+    //                    .headerFormatSet(@"Temperature")
+    ////                    .pointFormatSet(@"{point.x:%e %b, %Y} {point.y}:00: {point.value} ℃")
+    //            )
+    //                .turboThresholdSet(@1.7976931348623157e+308)
+    //            ]);
+    //}
+
+    public static AAOptions largeDataHeatmapChart() {
+        Map<String, Object> csvDataMap = AAOptionsCSV.csvData();
+        String csvStr = Objects.requireNonNull(csvDataMap.get("csv")).toString();
+        return new AAOptions()
+                .data(new AAData()
+                        .csv(csvStr)
+                        .parsed("function () {\n" +
+                                "                     start = +new Date();\n" +
+                                "                 }"))
+                .chart(new AAChart()
+                        .type(AAChartType.Heatmap)
+                        .margin(new Float[]{60f, 10f, 80f, 50f}))
+                .title(new AATitle()
+                        .text("大型热力图")
+                        .align(AAChartAlignType.Left)
+                        .x(40f))
+                .subtitle(new AASubtitle()
+                        .text("2013每天每小时的热力变化")
+                        .align(AAChartAlignType.Left)
+                        .x(40f))
+                .xAxis(new AAXAxis()
+                        .type(AAChartAxisType.Datetime)
+                        .min(1356998400000d)
+                        .max(1388534400000d)
+                        .labels(new AALabels()
+                                .align(AAChartAlignType.Left)
+                                .x(5f)
+                                .y(14f)
+                                .format("{value:%B}"))
+                        .tickLength(16f))
+                .yAxis(new AAYAxis()
+                        .title(new AAAxisTitle()
+                                .text((String) null))
+                        .labels(new AALabels()
+                                .format("{value}:00"))
+                        .minPadding(0f)
+                        .maxPadding(0f)
+                        .startOnTick(false)
+                        .endOnTick(false)
+                        .tickPositions(new Float[]{0f, 6f, 12f, 18f, 24f})
+                        .tickWidth(1f)
+                        .min(0f)
+                        .max(23f)
+                        .reversed(true))
+                .colorAxis(new AAColorAxis()
+                        .stops(new Object[][]{
+                                {0f, "#3060cf"},
+                                {0.5f, "#fffbbc"},
+                                {0.9f, "#c4463a"},
+                                {1f, "#c4463a"}
+                        })
+                        .min(-15f)
+                        .max(25f)
+                        .startOnTick(false)
+                        .endOnTick(false)
+                        .labels(new AALabels()
+                                .format("{value}℃")))
+                .series(new Object[]{
+                        new AAHeatmap()
+                                .borderWidth(0f)
+                                .colsize(86400000d)
+                                .tooltip(new AATooltip()
+                                        .headerFormat("Temperature")
+//                                        .pointFormat("{point.x:%e %b, %Y} {point.y}:00: {point.value} ℃")
+                                        )
+                                .turboThreshold(1.7976931348623157e+308d)
+                });
+
+                }
+
+    }
