c76927b28ab947d7cb4fa956052f866b27c44e27
An An <2236368544@qq.com>
2022-10-22 17:10:38 +0800

    Update AATooltip.java

--- aacharts/src/main/java/com/aachartmodel/aacharts/aaoptionsmodel/AATooltip.java
+++ aacharts/src/main/java/com/aachartmodel/aacharts/aaoptionsmodel/AATooltip.java
@@ -3,10 +3,10 @@ package com.aachartmodel.aacharts.aaoptionsmodel;
 import com.aachartmodel.aacharts.aatools.AAJSStringPurer;
 
 public class AATooltip {
-    public String backgroundColor;
+    public Object backgroundColor;
     public String borderColor;
-    public Float borderRadius;
-    public Float borderWidth;
+    public Number borderRadius;
+    public Number borderWidth;
     public AAStyle style;
     public Boolean enabled;
     public Boolean useHTML;
@@ -16,12 +16,16 @@ public class AATooltip {
     public String footerFormat;
     public Integer valueDecimals;
     public Boolean shared;
-    public Boolean crosshairs;
     public String valueSuffix;
-    public Boolean outside;
+    public Boolean followTouchMove;//https://api.highcharts.com.cn/highcharts#chart.panning
+    public Boolean shadow;
+    public Number padding;
+    public String pointFormatter;
+    public String positioner;
+    public AADateTimeLabelFormats dateTimeLabelFormats;
+    public String shape;
 
-
-    public AATooltip backgroundColor(String prop) {
+    public AATooltip backgroundColor(Object prop) {
         backgroundColor = prop;
         return this;
     }
@@ -31,12 +35,12 @@ public class AATooltip {
         return this;
     }
 
-    public AATooltip borderRadius(Float prop) {
+    public AATooltip borderRadius(Number prop) {
         borderRadius = prop;
         return this;
     }
 
-    public AATooltip borderWidth(Float prop) {
+    public AATooltip borderWidth(Number prop) {
         borderWidth = prop;
         return this;
     }
@@ -57,9 +61,7 @@ public class AATooltip {
     }
 
     public AATooltip formatter(String prop) {
-        String pureJSFunctionStr = "(" + prop + ")";
-        pureJSFunctionStr = AAJSStringPurer.pureJavaScriptFunctionString(pureJSFunctionStr);
-        formatter = pureJSFunctionStr;
+        formatter = AAJSStringPurer.pureAnonymousJSFunctionString(prop);
         return this;
     }
 
@@ -88,28 +90,52 @@ public class AATooltip {
         return this;
     }
 
-    public AATooltip crosshairs(Boolean prop) {
-        crosshairs = prop;
-        return this;
-    }
-
     public AATooltip valueSuffix(String prop) {
         valueSuffix = prop;
         return this;
     }
 
-    public AATooltip outside(Boolean prop) {
-        outside = prop;
+    public AATooltip followTouchMove(Boolean prop) {
+        followTouchMove = prop;
+        return this;
+    }
+
+    public AATooltip shadow(Boolean prop) {
+        shadow = prop;
+        return this;
+    }
+
+    public AATooltip padding(Number prop) {
+        padding = prop;
+        return this;
+    }
+
+    public AATooltip pointFormatter(String prop) {
+        pointFormatter = AAJSStringPurer.pureAnonymousJSFunctionString(prop);
+        return this;
+    }
+
+    public AATooltip positioner(String prop) {
+        positioner = AAJSStringPurer.pureAnonymousJSFunctionString(prop);
+        return this;
+    }
+
+    public AATooltip dateTimeLabelFormats(AADateTimeLabelFormats prop) {
+        dateTimeLabelFormats = prop;
+        return this;
+    }
+
+    public AATooltip shape(String prop) {
+        shape = prop;
         return this;
     }
 
     public AATooltip() {
         enabled = true;
         shared = true;
-        crosshairs = true;
     }
 
 
-    
-  
+
+
 }
